40cf886f7b2c956a78ae6c88f3446e0b55624e49
Parth Ganeriwala <46774949+ParthGaneriwala@users.noreply.github.com>
2025-04-01 16:41:57 -0400

    Added init translation for variable declaration

--- src/main/java/edu/fit/assist/translator/soar/Output.java
+++ src/main/java/edu/fit/assist/translator/soar/Output.java
@@ -1,11 +1,100 @@
 package edu.fit.assist.translator.soar;
 
+import java.util.ArrayList;
+
 public class Output {
     SoarRules rules;
     public Output(SoarRules rules){
         this.rules = rules;
     }
     public String generateOutput(){
-        return null;
+        String output = "dtmc\n\n";
+        rules.parseVariableValuePass();
+        output += generateVariableDeclarations();
+        output += generateModules();
+        return output;
+    }
+
+    private String generateModules() {
+        String output ="module ";
+        output += "endmodule";
+        return output;
+    }
+
+    private StringBuilder generateVariableDeclarations() {
+        StringBuilder output = new StringBuilder();
+
+
+        // Generate types for all variables :D
+        for(Variable var : rules.variables.values()){
+            var.generateType();
+            rules.mapNameToType.put(var.name, var.varType);
+        }
+
+
+        for(String startNode : rules.mapNameToType.keySet()){
+            if(rules.mapNameToType.get(startNode) == Variable.INVALID){
+                continue;
+            }
+            String node = startNode;
+            int startNodeType = rules.mapNameToType.get(startNode);
+            ArrayList<String> queue = new ArrayList<String>();
+            queue.add(node);
+            while(queue.size()>0){
+                String currentNode = queue.remove(0);
+                if(!rules.typeGraph.containsKey(currentNode)){
+                    continue;
+                }
+                for(String child : rules.typeGraph.get(currentNode)){
+                    if(!rules.mapNameToType.containsKey(child)){
+                        continue;
+                    }
+                    if(rules.mapNameToType.get(child) == Variable.INVALID){
+                        rules.mapNameToType.put(child,startNodeType);
+                        queue.add(child);
+                    }
+                }
+            }
+        }
+
+        for(String varName : rules.variables.keySet()){
+            Variable var = rules.variables.get(varName);
+            var.varType = rules.mapNameToType.get(var.name);
+            if(var.varType == Variable.INVALID){
+                var.varType = Variable.S_CONST;
+            }
+            rules.variables.put(varName,var);
+        }
+
+        for(Variable var : rules.variables.values()){
+            //var.generateType();
+            if(var.varType== Variable.INVALID){
+                var.varType = Variable.S_CONST;
+            }
+
+//            output.append("const ").append(var.varType).append(var.name).append(" = ");
+//            if(var.varType == Variable.S_CONST){
+//                output.append("{");
+//                boolean first = true;
+//                for(String varVal : var.values){
+//                    if(!first){
+//                        output.append(",");
+//                    }else{
+//                        first = false;
+//                    }
+//                    output.append(varVal);
+//                }
+//            }else if(var.varType == Variable.INT){
+//                output.append("integer;\n");
+//            }else if(var.varType == Variable.FLOAT){
+//                output.append("double;\n");
+//            }else{
+//                output.append("TYPE ERROR");
+//                System.err.println("TYPE ERROR with variable "+var.name);
+//            }
+//        }
+        return new StringBuilder(output + "\n\n");
+    }
+        return output;
     }
 }
--- src/main/java/edu/fit/assist/translator/soar/SoarRules.java
+++ src/main/java/edu/fit/assist/translator/soar/SoarRules.java
@@ -1,7 +1,66 @@
 package edu.fit.assist.translator.soar;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 public class SoarRules {
     ArrayList<Rule> rules;
+    LinkedHashMap<String, Variable> variables;
+    LinkedHashMap<String, Integer> mapNameToType;
+    LinkedHashMap<String, ArrayList<String>> typeGraph;
+
+    public void parseVariableValuePass(){
+        for (String varName: variables.keySet()){
+            Variable var = variables.get(varName);
+
+            for(int i=0; i<var.values.size(); i++){
+                String s = var.values.get(i);
+                if (s.startsWith("^VAR")){
+                    var.values.remove(i);
+                    i--;
+                    s = s.substring(4);
+                    for (String val : variables.get(s).values){
+                        if(!var.values.contains(val)){
+                            var.values.add(val);
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+    public void removeRule(String ruleName){
+
+        for(int i=0; i < rules.size(); i++){
+            if(rules.get(i).ruleName.equals(ruleName)){
+                rules.remove(i);
+                break;
+            }
+        }
+
+    }
+
+    public void addVariableValue(String varName, String varValue){
+        if(variables.containsKey(varName)){
+            variables.get(varName).addValue(varValue);
+        }else{
+            Variable v = new Variable(varName);
+            v.addValue(varValue);
+            variables.put(varName, v);
+        }
+    }
+
+
+    public void createNewRule(String name){
+        rules.add(new Rule(name, variables));
+    }
+
+    public Rule getRuleByName(String name){
+        for(Rule rule : rules){
+            if(rule.ruleName.equals(name)){
+                return rule;
+            }
+        }
+        return null;
+    }
 }
